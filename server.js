6b8514f33a8d8d0ce06a147e0ecf0876efd257b5
Pallavi Vetal <vetalpallavi21@gmail.com>
2017-02-03 00:54:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -131,13 +131,6 @@ app.get('/^article-',function(req,res){
  res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two',function(req,res){
- res.send("Article two will be served here");
-});
-
-app.get('/article-three',function(req,res){
- res.send("Article three will be served here");
-});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
