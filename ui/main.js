e1fdcdc1c35fda6635bd94bbe50203722937728e
Pallavi Vetal <vetalpallavi21@gmail.com>
2017-02-05 02:08:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,18 @@ var margin = 0;
 function moveRight(){
     margin = margin + 10;
     profile.style.marginLeft = margin + 'px';
+    }
+}
+function moveDown(){
+    margin = margin +10;
+    profile.style.marginRight = margin + 'px';
 }
 profile.onclick = function(){
-    var interval =setInterval(moveRight,50);
+    if(profile.style.marginLeft < 50){
+        var interval =setInterval(moveRight,50); 
+    }
+    else{
+        margin = 0;
+        var inter =setInterval(moveDown,50);
+    }
 };
