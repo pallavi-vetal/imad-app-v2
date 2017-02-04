176885e0f34615804a40763c7a81982ed4578060
Pallavi Vetal <vetalpallavi21@gmail.com>
2017-02-05 01:07:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
 console.log('Loaded!');
+
+var profile = document.getElementById("propic");
+var margin = 0;
+function moveRight(){
+    margin = margin + 10;
+    profile.style.marginLeft = margin + "px";
+}
+profile.onClick = function(){
+    var interval =setInterval(moveRight,50);
+};
