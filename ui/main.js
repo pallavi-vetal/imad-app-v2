2b0d4ce917f0331f62920d601a3e4bb369250101
Pallavi Vetal <vetalpallavi21@gmail.com>
2017-02-07 23:17:46 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -23,3 +23,28 @@ function moveDown(){
 profile.onclick = function(){
    var inter = setInterval(moveRight,100);
 };
+
+				var button=document.getElementById('counter');
+				button.onclick=function() {
+				  
+				 
+				  
+				  var request=new XMLHttpRequest();
+				  
+				  
+				  
+				  request.onreadystatechange=function(){
+				    if(request.readyState===XMLHttpRequest.DONE){
+					if(request.status===200){
+					    var counter=request.responseText;
+					   
+					    var span=document.getElementById('count');
+					    span.innetHTML=counter.toString();
+					 }
+				   }
+				      
+				  };
+		              request.open('GET','http://pallavi-vetal.imad.hasura-app.io/counter',true);
+			   
+			      request.send(null); 
+				};
