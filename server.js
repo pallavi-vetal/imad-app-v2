0ecf8af2993b46bf7f2a6a7f9458f010fb034c4b
Pallavi Vetal <vetalpallavi21@gmail.com>
2017-02-12 14:01:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/1.jpg', function (req, res) {
+app.get('/ui/1.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', '1.jpg'));
 });
 app.get('/main.js', function (req, res) {
