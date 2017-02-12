3b8ec117ef2bb8240455f59d80609e25f431196a
Pallavi Vetal <vetalpallavi21@gmail.com>
2017-02-12 18:04:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -33,11 +33,40 @@ like_btn.onclick=function(){
 
 //************************* For Searching Operation *********************/
 
-var search_input = document.getElementById('search-input');
-var search_val = search_input.value;
+
 
 var search_btn = document.getElementById('search-btn');
 search_btn.onclick = function(){
-  
+    
+    //create request object
+    var list = ' ';
+    var request = new XMLHttpRequest();
+    
+     //capture and store response into a variable
+     
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                //capture the list of names
+                var names = request.responseText;
+                names = JSON.parse(names);
+                for(var i=0;i<names.length;i++){
+                    list+='<li'> + names[i] + '</li>';
+                    
+                }
+                var search_ul=document.getElementById('search-ul');
+                ul.innerHTML = list; 
+                
+            }
+        }
+        
+    };
+   
+    
+    //make a request to counter endpoint
+    var search_input = document.getElementById('search-input');
+    var search_val = search_input.value;
+    request.open('GET',"http://pallavi-vetal.imad.hasura-app.io/search-submit?name="+search_input,true);
+    request.send(null);
       
 };
