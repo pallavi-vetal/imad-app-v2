de799fe2848996c9b3bc30b26615cead948985c5
Pallavi Vetal <vetalpallavi21@gmail.com>
2017-02-12 17:10:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,28 @@
 console.log('Loaded!');
 var like_btn=document.getElementById('like-btn');
+like_btn.onclick=function(){
+  
+    //create request object
+    
+    var request = new XMLHttpRequest();
+    
+     //capture and store response into a variable
+     
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+                
+            }
+        }
+        
+    };
+   
+    
+    //make a request to counter endpoint
+    
+    request.open("http://pallavi-vetal.imad.hasura-app.io/counter",true);
+    request.send(null);
+};
