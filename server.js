8de87c7620f4bc6d528a606c97ca7db0c82ee11a
Pallavi Vetal <vetalpallavi21@gmail.com>
2017-02-12 14:24:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,6 +72,17 @@ function createTemplate(data)
 					<!DOCTYPE html>
 					<html>
 					<head>
+                    	  <title>${title}</title>
+                           <!-- Bootstrap -->
+                         	<!-- Latest compiled and minified CSS -->
+                    		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+                    		
+                    		<!-- jQuery library -->
+                    		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+                    		<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+                            
+                    		<link rel="stylesheet" type="text/css" href="ui/style.css">
+                    	
 						<title>${title}</title>
 						<!-- Latest compiled and minified CSS -->
 						<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
