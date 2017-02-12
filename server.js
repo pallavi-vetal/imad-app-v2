58ca16a0a79caa3a73b8333259b834c23355647d
Pallavi Vetal <vetalpallavi21@gmail.com>
2017-02-12 14:25:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ function createTemplate(data)
                 			        <span class="icon-bar"></span>                        
                 			      </button>
                 		      	  <div class="navbar-header">
-                		      		<a href="#" class="navbar-brand"><span><img src="madi.png" width="30px">${heading}</span></a>
+                		      		<a href="#" class="navbar-brand"><strong>${heading}</strong></a>
                 		      	  </div>
                 		      	  <div class="collapse navbar-collapse" id="myNavbar">
                 		      		<ul class="nav navbar-nav">
