ca1199e235792dc355a0033f16c2de4bec06b9b1
Pallavi Vetal <vetalpallavi21@gmail.com>
2017-02-12 17:57:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,6 @@
 console.log('Loaded!');
+
+//****************************For counting likes*****************************
 var red= '#FF69B4';
 var like_btn=document.getElementById('like-btn');
 like_btn.onclick=function(){
@@ -29,3 +31,13 @@ like_btn.onclick=function(){
     request.send(null);
 };
 
+//************************* For Searching Operation *********************/
+
+var search_input = document.getElementById('search-input');
+var search_val = search_input.value;
+
+var search_btn = document.getElementById('search-btn');
+search_btn.onclick = function(){
+  
+      
+};
