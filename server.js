bbed75097da00ed56c733e1d66417a70890dbfb2
Pallavi Vetal <vetalpallavi21@gmail.com>
2017-02-22 23:46:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 var articles = {
